b908d538c54c3c7d654333df76a8ef16228bca45
Syed-Mubeen-11 <syedmubeen422@gmail.com>
2026-05-19 23:15:37 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8080/api'
+// const API_BASE_URL = 'http://localhost:8080/api'
+const API_BASE_URL = 'https://college-event-management-t6fj.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
