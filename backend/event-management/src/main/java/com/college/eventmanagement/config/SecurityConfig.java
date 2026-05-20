c14bbb40b69b9fc87ae8a8181dc19df4953d83a4
Syed-Mubeen-11 <syedmubeen422@gmail.com>
2026-05-20 06:57:25 +0530

    Update CORS with main production URL

--- backend/event-management/src/main/java/com/college/eventmanagement/config/SecurityConfig.java
+++ backend/event-management/src/main/java/com/college/eventmanagement/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
             "http://localhost:5173",
             "http://localhost:3000",
-            "https://college-event-management-rho.vercel.app"
+            "https://college-event-management-orpin.vercel.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
